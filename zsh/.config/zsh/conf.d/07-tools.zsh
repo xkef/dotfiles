2009bc104a40f3b98c0f29a2e21302a19da41212
xkef <git@xkef.dev>
2026-03-05 21:39:24 +0100

    refactor(shell): use Starship native command duration tracking

--- zsh/.config/zsh/conf.d/07-tools.zsh
+++ zsh/.config/zsh/conf.d/07-tools.zsh
@@ -12,25 +12,6 @@ if command -v zoxide &>/dev/null; then
   eval "$(zoxide init zsh --cmd cd)"
 fi
 
-# Command duration tracking (exported for Starship env_var module)
-_cmd_timer_preexec() { _cmd_timer_start=$EPOCHREALTIME }
-_cmd_timer_precmd() {
-  if [[ -n $_cmd_timer_start ]]; then
-    local ms=$(( int(($EPOCHREALTIME - _cmd_timer_start) * 1000) ))
-    if (( ms < 1000 )); then
-      export PROMPT_DURATION="${ms}ms"
-    else
-      export PROMPT_DURATION="$(printf '%.1fs' $(( ms / 1000.0 )))"
-    fi
-    _cmd_timer_start=
-  else
-    unset PROMPT_DURATION
-  fi
-}
-autoload -Uz add-zsh-hook
-add-zsh-hook preexec _cmd_timer_preexec
-add-zsh-hook precmd _cmd_timer_precmd
-
 # Starship prompt
 if command -v starship &>/dev/null; then
   eval "$(starship init zsh)"
