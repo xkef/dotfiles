149f62dcebf2f445bcebd1086ecb7baca2a757a4
xkef <git@xkef.dev>
2026-03-15 18:57:57 +0100

    feat(nvim): add and configure diffview.nvim

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -10,12 +10,48 @@ return {
         local map = function(mode, l, r, desc)
           vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
         end
+
         map("n", "]h", gs.next_hunk, "Next hunk")
         map("n", "[h", gs.prev_hunk, "Prev hunk")
         map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
+
+        map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
         map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+        map("v", "<leader>hs", function()
+          gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, "Stage selection")
+        map("v", "<leader>hr", function()
+          gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, "Reset selection")
+        map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
+        map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
+        map("n", "<leader>hu", function()
+          gs.stage_hunk({ vim.fn.line("."), vim.fn.line(".") }, { staged = true })
+        end, "Unstage hunk")
+
         map("n", "<leader>hd", gs.diffthis, "Diff buffer")
+        map("n", "<leader>hD", function()
+          gs.diffthis("~")
+        end, "Diff against last commit")
+        map("n", "<leader>hb", function()
+          gs.blame_line({ full = true })
+        end, "Blame line (full)")
       end,
+    },
+  },
+
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diff view" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File history" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Branch history" },
+    },
+    opts = {
+      view = {
+        merge_tool = { layout = "diff3_mixed" },
+      },
     },
   },
 }
