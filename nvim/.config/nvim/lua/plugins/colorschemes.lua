f2f76897f6b6967aa10ebf8b23b14ceec99dd34f
xkef <git@xkef.dev>
2026-03-15 22:06:07 +0100

    feat(theme): enhance colorscheme support and tmux style updates

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -6,20 +6,53 @@ return {
     opts = {
       integrations = {
         blink_cmp = true,
+        diffview = true,
+        flash = true,
         gitsigns = true,
+        grug_far = true,
+        lsp_trouble = true,
         mason = true,
-        native_lsp = { enabled = true },
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        snacks = true,
         treesitter = true,
-        lualine = true,
+        which_key = true,
       },
     },
   },
   { "folke/tokyonight.nvim", lazy = true },
-  { "rose-pine/neovim", name = "rose-pine", lazy = true },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+    opts = { variant = "auto" },
+  },
   { "ellisonleao/gruvbox.nvim", lazy = true },
   { "shaunsingh/nord.nvim", lazy = true },
   { "olimorris/onedarkpro.nvim", lazy = true },
-  { "rebelot/kanagawa.nvim", lazy = true },
-  { "neanias/everforest-nvim", lazy = true },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    opts = {
+      background = {
+        dark = "wave",
+        light = "lotus",
+      },
+    },
+  },
+  {
+    "neanias/everforest-nvim",
+    lazy = true,
+    opts = {
+      background = "hard",
+    },
+  },
   { "Mofiqul/dracula.nvim", lazy = true },
 }
