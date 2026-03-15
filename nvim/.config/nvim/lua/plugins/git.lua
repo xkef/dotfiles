a8165233511d01b7444c475d4592b0c389cafa41
xkef <git@xkef.dev>
2026-03-15 19:43:36 +0100

    feat(nvim): update git keymaps for undoing staged hunks

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -25,9 +25,7 @@ return {
         end, "Reset selection")
         map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
         map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
-        map("n", "<leader>hu", function()
-          gs.stage_hunk({ vim.fn.line("."), vim.fn.line(".") }, { staged = true })
-        end, "Unstage hunk")
+        map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
 
         map("n", "<leader>hd", gs.diffthis, "Diff buffer")
         map("n", "<leader>hD", function()
