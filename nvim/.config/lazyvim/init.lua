dac38f33187cd58499d5718951b58b59f288c2fb
xkef <git@xkef.dev>
2026-03-28 12:11:52 +0100

    feat(nvim): add TypeScript language support

--- nvim/.config/lazyvim/init.lua
+++ nvim/.config/lazyvim/init.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
